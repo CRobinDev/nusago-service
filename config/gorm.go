b4c7ff04e06940e5ace2338796f2292404894259
Christopher Robin <williamchen1506@gmail.com>
2025-02-22 01:36:18 +0700

    `refactor:` cleaning code

--- config/gorm.go
+++ config/gorm.go
@@ -9,16 +9,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// func loadDSN() string {
-// 	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-// 		os.Getenv("DB_HOST"),
-// 		os.Getenv("DB_USER"),
-// 		os.Getenv("DB_PASSWORD"),
-// 		os.Getenv("DB_NAME"),
-// 		os.Getenv("DB_PORT"),
-// 	)
-// }
-
 func NewDB() (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(os.Getenv("POSTGRES_DSN")), &gorm.Config{
 		PrepareStmt: true,
